5a398f3d6def3f0f0bb556e8b5bca8b067e7a859
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2020-02-05 07:18:14 +0200

    Update AddToCalendar.py

--- AddToCalendar.py
+++ AddToCalendar.py
@@ -1,5 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from apiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 import pprint
 pp = pprint.PrettyPrinter()
 # use creds to create a client to interact with the Google Drive API
@@ -8,6 +10,14 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', s
 client = gspread.authorize(creds)
 
 sheet = client.open("Spring20 Schedule").sheet1
+
+
+
+scopes = ['https://www.googleapis.com/auth/calendar']
+flow = InstalledAppFlow.from_client_secrets_file("client_secret1.json", scopes=scopes)
+credentials = flow.run_console()
+
+CAL = build("calendar", "v3", credentials=credentials)
 
 list_of_hashes = sheet.get_all_records()
 pp.pprint(list_of_hashes[0])
@@ -29,32 +39,43 @@ cid = {'CSCI221':'Peacock',
        'CSCI463L':'Tomato',
        'CSCI467':'Banana',
        'CSCI467T':'Banana'}
-for i in range(len(list_of_hashes)):
-    print("***%s"%list_of_hashes[i][''])
-    for k, v in list_of_hashes[i].items():
-        if(v!= '' and k!=''):        
-            title = v        
-            start_time = k
-            end_time = str(int(start_time.split(':')[0]) + 1) + ':' + str(int(start_time.split(':')[1]) - 10)
-            ddate = dd[list_of_hashes[i]['']]
-            color_id = cid[v]
-            print('''Title: %s
-                  Start: %s 
-                  End: %s
-                  Date: 2020-02-%s
-                  Color: %s'''%(title,start_time,end_time,ddate,color_id))
+def setEvent(list_of_hashes,dd,cid):
+    GMT_OFF = '+02:00'
+    for i in range(len(list_of_hashes)):
+        print("***%s"%list_of_hashes[i][''])
+        for k, v in list_of_hashes[i].items():
+            if(v!= '' and k!=''):        
+                title = v        
+                start_time = k
+                end_time = str(int(start_time.split(':')[0]) + 1) + ':' + str(int(start_time.split(':')[1]) - 10)
+                ddate = dd[list_of_hashes[i]['']]
+                color_id = cid[title]
+                print('''Title: %s
+                      Start: %s 
+                      End: %s
+                      Date: 2020-02-%s
+                      Color: %s'''%(title,start_time,end_time,ddate,color_id))
+                return title, start_time, end_time, ddate, color_id
     
-'''       
+      
+#Test
+title = 'MATH205Test'
+start_time = '10:30'
+end_time = '11:30'
+ddate = dd[list_of_hashes[0]['']]
+color_id = cid['MATH205']
 #Event Format
-GMT_OFF = '+02:00'      
+GMT_OFF = '+02:00'  
 event = {
         'summary': title,
-        'start':{'dateTime':'2020-02-%sT%s:00.000%s'%(ddate,start_time,GMT_OFF)},
-        'end':{'dateTime':'2020-02-%sT%s:00.000%s'%(ddate,end_time,GMT_OFF)},
-        'recurrence': [
-                'RRULE:FREQ=WEEKLY;UNTIL=20200705T170000Z',
-                ],
-        "colorId": color_id,
-        }        
-'''
+        'start':{'dateTime':'2020-02-%sT%s:00.000%s'%(ddate,start_time,GMT_OFF),'timeZone': 'Africa/Cairo'},
+        'end':{'dateTime':'2020-02-%sT%s:00.000%s'%(ddate,end_time,GMT_OFF),'timeZone': 'Africa/Cairo'},
+        'recurrence': ['RRULE:FREQ=WEEKLY;UNTIL=20200216T170000Z',]}        
+recurring_event = CAL.events().insert(calendarId='primary', body=event).execute()
+
+print('''*** %s event added:
+    Start: %s
+    End: %s'''%(recurring_event['summary'],
+    recurring_event['start']['dateTime'],recurring_event['end']['dateTime']))
+
 
