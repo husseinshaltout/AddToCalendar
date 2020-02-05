5240eebcd42500d61accfe4e5a119aab26b5d6d9
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2020-02-05 05:45:13 +0200

    Update AddToCalendar.py

--- AddToCalendar.py
+++ AddToCalendar.py
@@ -9,13 +9,7 @@ client = gspread.authorize(creds)
 
 sheet = client.open("Spring20 Schedule").sheet1
 
-col = sheet.col_values(1)
 list_of_hashes = sheet.get_all_records()
-header = sheet.row_values(1)
-l = sheet.row_values(2)
-pp.pprint(header)
-pp.pprint(l)
-
 pp.pprint(list_of_hashes[0])
 
 #Date dictionary
@@ -25,45 +19,34 @@ dd = {'SUN':'09',
       'WED':'12', 
       'THU':'13'}        
 #Color id dictionary
-cid = {'CSCI221':'',
-       'CSCI221T2':'',
+cid = {'CSCI221':'Peacock',
+       'CSCI221T2':'Peacock',
+       'CSCI221L1':'Peacock',
        'MATH205':'Basil',
-       'MATH205T1':'Basil',
+       'MATH205T2':'Basil',
        'CSCI463':'Tomato',
        'CSCI463T':'Tomato',
        'CSCI463L':'Tomato',
-       'CSCI467':'',
-       'CSCI467T':''}
-#Test dictionary
-td = {'': 'SUN',
- '8:30': '',
- '9:30': '',
- '10:30': '',
- '11:30': 'MATH205',
- '12:30': 'MATH205',
- '13:30': '',
- '14:30': 'CSCI221',
- '15:30': 'CSCI221'}
-       
-for k, v in td.items():
-    if(v!= '' and k!=''):        
-        title = v        
-        start_time = k
-        end_time = str(int(start_time[:2]) + 1) + ':' + str(int(start_time[3:]) - 10)
-        ddate = dd[list_of_hashes[0]['']]
-        print('''Title: %s
-              Start: %s 
-              End: %s
-              Date: 2020-02-%s'''%(title,start_time,end_time,ddate))
-        
-''' 
-#Formating
-title = l3[0][1]        
-start_time = l3[0][0]
-end_time = str(int(start_time[:2]) + 1) + ':' + str(int(start_time[3:]) - 10)
-GMT_OFF = '+02:00'
-color_id  = 'Basil'
-ddate = dd[list_of_hashes[0]['']]         
+       'CSCI467':'Banana',
+       'CSCI467T':'Banana'}
+for i in range(len(list_of_hashes)):
+    print("***%s"%list_of_hashes[i][''])
+    for k, v in list_of_hashes[i].items():
+        if(v!= '' and k!=''):        
+            title = v        
+            start_time = k
+            end_time = str(int(start_time.split(':')[0]) + 1) + ':' + str(int(start_time.split(':')[1]) - 10)
+            ddate = dd[list_of_hashes[i]['']]
+            color_id = cid[v]
+            print('''Title: %s
+                  Start: %s 
+                  End: %s
+                  Date: 2020-02-%s
+                  Color: %s'''%(title,start_time,end_time,ddate,color_id))
+    
+'''       
+#Event Format
+GMT_OFF = '+02:00'      
 event = {
         'summary': title,
         'start':{'dateTime':'2020-02-%sT%s:00.000%s'%(ddate,start_time,GMT_OFF)},
